81ed840171eff5e33cb640603b4ebc79f1d552a8
foo87b <foo87b@users.noreply.github.com>
2018-06-14 07:09:13 +0900

    fix p2p_compactblocks.py

--- test/functional/p2p_compactblocks.py
+++ test/functional/p2p_compactblocks.py
@@ -256,11 +256,12 @@ class CompactBlocksTest(BitcoinTestFramework):
         node.generate(101)
         num_transactions = 25
         address = node.getnewaddress()
-        validate = node.validateaddress(address)
-        if use_witness_address and not validate["iswitness"]:
+        if use_witness_address:
             # Want at least one segwit spend, so move all funds to
             # a witness address.
-            address = node.addwitnessaddress(address)
+            validate = node.validateaddress(address)
+            if not validate["iswitness"]:
+                address = node.addwitnessaddress(address)
             value_to_send = node.getbalance()
             node.sendtoaddress(address, satoshi_round(value_to_send-Decimal(0.1)))
             node.generate(1)
