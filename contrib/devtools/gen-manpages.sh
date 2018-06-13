866ad746d4580d785eed2ba20c95b759f7d6a234
chipstar <oobayashi@gmail.com>
2018-06-13 21:00:24 +0900

    BTC to XPC

--- contrib/devtools/gen-manpages.sh
+++ contrib/devtools/gen-manpages.sh
@@ -12,7 +12,7 @@ XPCHAINQT=${XPCHAINQT:-$SRCDIR/qt/xpchain-qt}
 [ ! -x $XPCHAIND ] && echo "$XPCHAIND not found or not executable." && exit 1
 
 # The autodetected version git tag can screw up manpage output a little bit
-BTCVER=($($XPCHAINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
+XPCVER=($($XPCHAINCLI --version | head -n1 | awk -F'[ -]' '{ print $6, $7 }'))
 
 # Create a footer file with copyright content.
 # This gets autodetected fine for xpchaind if --version-string is not set,
@@ -22,8 +22,8 @@ $XPCHAIND --version | sed -n '1!p' >> footer.h2m
 
 for cmd in $XPCHAIND $XPCHAINCLI $XPCHAINTX $XPCHAINQT; do
   cmdname="${cmd##*/}"
-  help2man -N --version-string=${BTCVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
-  sed -i "s/\\\-${BTCVER[1]}//g" ${MANDIR}/${cmdname}.1
+  help2man -N --version-string=${XPCVER[0]} --include=footer.h2m -o ${MANDIR}/${cmdname}.1 ${cmd}
+  sed -i "s/\\\-${XPCVER[1]}//g" ${MANDIR}/${cmdname}.1
 done
 
 rm -f footer.h2m
